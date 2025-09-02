d9d262d76252f541b17ba3171af2c28827bc5845
Georges Nodari <229836467+itsnoge@users.noreply.github.com>
2025-09-03 01:51:35 +0200

    feat(posts): set page metadata title for PostsPage

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -1,7 +1,25 @@
+import type { Metadata } from 'next';
 import { sanityFetch } from '@/sanity/lib/live';
 import { POST_QUERY } from '@/sanity/lib/queries';
 import { notFound } from 'next/navigation';
 import { Post } from '@/components/Post';
+
+type RouteProps = {
+  params: Promise<{ slug: string }>;
+};
+
+export async function generateMetadata({
+  params,
+}: RouteProps): Promise<Metadata> {
+  const { data: post } = await sanityFetch({
+    query: POST_QUERY,
+    params: await params,
+  });
+
+  return {
+    title: post?.title,
+  };
+}
 
 export default async function PostDetailsPage({
   params,
